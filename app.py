fffca9360783ceec95f54a7b4acb84fb888f5dac
Neftalem <dneftalem@gmail.com>
2025-08-27 16:33:28 +0300

    finsh manu

--- app.py
+++ app.py
@@ -19,5 +19,26 @@ class ATM:
         self.balanc -= amount
         print(f'You withdraw {amount}, Curent balance {self.balanc}')
 
+    def manu(self):
+        while True:
+            print("\n--- ATM Menu ---")
+            print('1.Show balanc')
+            print('2.Deposit')
+            print('3.withdraw')
+            print("4. Exit")
+            choice = input("Choose one: ")
+
+            if choice == '1':
+                self.show_balance()
+            elif choice == '2':
+                self.deposit()
+            elif choice == '3':
+                self.withdraw()
+            elif choice == '4':
+                print("Exiting... Thank you for using the ATM!")
+                break
+            else:
+                print("Invalid choice, try again.")
+
 atm1 = ATM()
-atm1.withdraw()
+atm1.menu()
