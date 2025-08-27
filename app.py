893a6b600b19561c962da5d624fdeebf34a03297
Neftalem <dneftalem@gmail.com>
2025-08-27 16:24:34 +0300

    finsh withdrow

--- app.py
+++ app.py
@@ -5,13 +5,19 @@ class ATM:
         self.balanc = balanc
     
     def show_balanc(self):
-        print(self.balanc)
+        print(f'Curent balanc {self.balanc}')
     
     def Deposit(self, amount=None):
         if amount is None:
             amount = int(input('Enter amount: '))
         self.balanc += amount
         print(f'You Deposit {amount} curent balanc {self.balanc}')
+    
+    def withdraw(self, amount=None):
+        if amount is None:
+            amount = int(input('Enter amount: '))
+        self.balanc -= amount
+        print(f'You withdraw {amount}, Curent balance {self.balanc}')
 
 atm1 = ATM()
-atm1.Deposit()
+atm1.withdraw()
