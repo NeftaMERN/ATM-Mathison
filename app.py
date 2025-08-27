98a8269029436a6378f8fbfafa946e409fcaf383
Neftalem <dneftalem@gmail.com>
2025-08-27 16:20:01 +0300

    add input

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ class ATM:
     def show_balanc(self):
         print(self.balanc)
     
-    def Deposit(self, amount):
+    def Deposit(self, amount=None):
+        if amount is None:
+            amount = int(input('Enter amount: '))
         self.balanc += amount
         print(f'You Deposit {amount} curent balanc {self.balanc}')
 
 atm1 = ATM()
-atm1.Deposit(100)
+atm1.Deposit()
